ab5363af124c21b3a91322c02d344315055f5eb3
RageBill <kelvin@firstcodeacademy.com>
2017-09-23 10:50:45 +0800

    Finished Game

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+	int nextIndex;
+
+	public Transform[] targets;
+
+	public float speed = 1;
+
+	bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = targets[0].position;
+
+		nextIndex = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleInput ();
+
+		HandleMovement ();
+	}
+
+	void HandleMovement(){
+
+		if (!isMoving)
+			return;
+
+		float distance = Vector3.Distance (transform.position, targets[nextIndex].position);
+
+		if (distance > 0) {
+			float movement = speed * Time.deltaTime;
+
+			transform.position = Vector3.MoveTowards (transform.position, targets [nextIndex].position, movement);
+		} else {
+
+			nextIndex++;
+
+			isMoving = false;
+
+			if (nextIndex >= targets.Length) {
+				nextIndex = 0;
+			}
+
+		}
+	}
+
+	void HandleInput(){
+		if (Input.GetButtonDown ("Fire1")) {
+			isMoving = !isMoving;
+		}
+	}
+}
